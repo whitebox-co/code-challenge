c7e17ef22beb91214ac1bc9d3653069f1b227e6a
lirikf <darrylmost@gmail.com>
2019-02-14 16:16:38 -0500

    getSingle endpoint running

--- server.js
+++ server.js
@@ -8,8 +8,32 @@ const jsonUrl = "https://next.json-generator.com/api/json/get/EkzBIUWNL";
 
 
 app.use(express.static('public'))
+
+// get many endpoint
 app.get('/getMany',(req,res)=>{
-    request.get(jsonUrl).pipe(res);
+    try{
+	request.get(jsonUrl).pipe(res);
+    }
+    catch(err){
+	res.json({"Error":err});
+    }
+});
+
+// get single endpoint
+app.get('/getSingle/:id',(req,res)=>{
+    let id = req.params.id;
+    let params = {
+	url:jsonUrl,
+	json:true
+    }
+    request(params,(err,response,body)=>{
+	if(err || response.statusCode !== 200){
+	    res.json({"Error":err});
+	}
+	else{
+	    res.send(body[id]);
+	}
+    });
 });
 
 app.listen(port,()=>console.log('app listening on ' + port));
