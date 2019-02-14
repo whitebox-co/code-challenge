16e9d06d457a054bc5b1421a273e6f4366983425
lirikf <darrylmost@gmail.com>
2019-02-14 17:14:35 -0500

    implemented pricing filter function

---
+++ public/js/filter.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+    $('#filter').click(function(){
+	let $lower = Number($('#value-lower').text());
+	let $upper = Number($('#value-upper').text());
+	$(".p-r-5").each(function(index){
+	    let $price = $(this).text().trim().substring(1);
+	    let $product = $(this).closest(".block2");
+	    if($price < $lower || $price > $upper){
+		$product.toggle();
+	    }
+	    else{
+		if($product.is(":hidden")){
+		    $product.toggle();
+		}
+	    }
+	});
+    });
+});
