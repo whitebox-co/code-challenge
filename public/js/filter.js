d9ea0a8ce522a6ad46c4b713309f4be7fcec61cf
lirikf <darrylmost@gmail.com>
2019-02-14 23:43:46 -0500

    improved functionality and commenting

--- public/js/filter.js
+++ public/js/filter.js
@@ -2,12 +2,33 @@ $(document).ready(function(){
     $filter = null;
     $firstSelect = $('.selection-2')[0];
     $secondSelect = $('.selection-2')[1];
+    $defaultOrder = null;
 
+    // retrieves the latest order of products
+    let latestProducts = function(){
+	$order = [];
+	$(".block2").closest('.p-b-50').each(function(index){
+	    $order.push($(this));
+	});
+	return $order;
+    }
+
+    $defaultOrder = latestProducts(); // default order of products
+	
     $("button").each(function(index){
 	if($(this).text().trim() === "Filter"){
 	    $filter = $(this);
 	}
     });
+
+    // reroutes user to detailed product page on click of product
+    $('.block2-overlay').each(function(index){
+	$(this).click(function(){
+	    window.location.href="http://localhost:3000/detail";
+	});
+    });
+
+    // handles filter functionality
     $filter.click(function(){
 	let $lower = Number($('#value-lower').text());
 	let $upper = Number($('#value-upper').text());
@@ -26,9 +47,10 @@ $(document).ready(function(){
 	    }
 	});
     });
+
+    // handles select functionality
     $('select').on('change',function(){
 	if(this.value === "Price: low to high"){
-	    console.log('lowhigh');
 	    let orderDef = $(".block2").closest('.p-b-50').sort(function(a,b){
 		let $first = Number($(a).find('.block2-price').text().trim().substring(1));
 		let $second = Number($(b).find('.block2-price').text().trim().substring(1));
@@ -44,8 +66,32 @@ $(document).ready(function(){
 	    });
 	    $(".block2").closest('.row').html(orderDef);
 	}
-
+	if(this.value === "Default Sorting"){
+	    $(".block2").closest('.row').html($defaultOrder);
+	}
     });
+
+
+    // handles product pages
+    let $firstPage = null;
+    let $secondPage = null;
+    $('a').each(function(index){
+	if($(this).text() === '1'){
+	    $firstPage = $(this);
+	    $(this).click(function(){
+		$secondPage.removeClass('active-pagination');
+		$firstPage.addClass('active-pagination');
+	    });
+	}
+	if($(this).text() === '2'){
+	    $secondPage = $(this);
+	    $(this).click(function(){
+		$firstPage.removeClass('active-pagination');
+		$secondPage.addClass('active-pagination');
+	    });
+	}
+    });
+
 });
 
 
