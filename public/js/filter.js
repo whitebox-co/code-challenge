bae6345151ad094da565102fd3b697b829abd2aa
lirikf <darrylmost@gmail.com>
2019-02-14 23:11:34 -0500

    working on selection

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
     $filter = null;
+    $firstSelect = $('.selection-2')[0];
+    $secondSelect = $('.selection-2')[1];
+
     $("button").each(function(index){
 	if($(this).text().trim() === "Filter"){
 	    $filter = $(this);
@@ -10,7 +13,7 @@ $(document).ready(function(){
 	let $upper = Number($('#value-upper').text());
 	$(".p-r-5").each(function(index){
 	    let $price = $(this).text().trim().substring(1);
-	    let $product = $(this).closest(".block2");
+	    let $product = $(this).closest(".p-b-50");
 	    if($price < $lower || $price > $upper){
 		if(!$product.is(":hidden")){
 		    $product.toggle();
@@ -22,6 +25,26 @@ $(document).ready(function(){
 		}
 	    }
 	});
+    });
+    $('select').on('change',function(){
+	if(this.value === "Price: low to high"){
+	    console.log('lowhigh');
+	    let orderDef = $(".block2").closest('.p-b-50').sort(function(a,b){
+		let $first = Number($(a).find('.block2-price').text().trim().substring(1));
+		let $second = Number($(b).find('.block2-price').text().trim().substring(1));
+		return $first > $second;
+	    });
+	    $(".block2").closest('.row').html(orderDef);
+	}
+	if(this.value === "Price: high to low"){
+	    let orderDef = $(".block2").closest('.p-b-50').sort(function(a,b){
+		let $first = Number($(a).find('.block2-price').text().trim().substring(1));
+		let $second = Number($(b).find('.block2-price').text().trim().substring(1));
+		return $first < $second;
+	    });
+	    $(".block2").closest('.row').html(orderDef);
+	}
+
     });
 });
 
