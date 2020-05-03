6194563dfb7a41d35613664ed96cbc45b4ab1339
Darren Napier <dnaps@Darrens-MacBook-Pro.local>
2020-05-03 11:02:32 -0700

    working update to header

--- public/js/product.filter.js
+++ public/js/product.filter.js
@@ -1,3 +1,25 @@
-document.onload = function () {
-  
+window.onload = function () {
+  // ### makes querySelector easier to use ###
+  const $ = (selector, context = document) => context.querySelector(selector);
+  const $$ = (selector, context = document) =>
+    context.querySelectorAll(selector);
+
+  // ### Product Page Header Updates ###
+  const productPageHeader = $('h2');
+  const productPageSubHeader = $('h2 + p');
+  const categories = $$('.p-t-4 > a.s-text13');
+
+  categories.forEach((cat) =>
+    cat.addEventListener('click', function updateHeader(e) {
+      console.log('Changing the header');
+      const titleChange = e.target.innerText;
+      const all = `See ${titleChange} Our New Arrivals In The 2020 Collection!`;
+      const others = `New Arrivals ${titleChange} Collection 2020`;
+      productPageHeader.innerHTML = titleChange;
+
+      titleChange === 'All'
+        ? (productPageSubHeader.innerHTML = all)
+        : (productPageSubHeader.innerHTML = others);
+    })
+  );
 };
